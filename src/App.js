121d6d1935639a0ea0b34997bcb115929fe7174e
parsanithin5 <pparsanithin@gmail.com>
2025-02-05 11:43:47 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -58,25 +58,88 @@
 
 //! component composition examples
 
-import React from "react";
-import Main from "./components/Main";
-import Navbar from "./components/Navbar";
-import Sidebar1 from "./components/Sidebar1";
-import Sidebar2 from "./components/Sidebar2";
-import Footer from "./components/Footer";
+// import React from "react";
+// import Main from "./components/Main";
+// import Navbar from "./components/Navbar";
+// import Sidebar1 from "./components/Sidebar1";
+// import Sidebar2 from "./components/Sidebar2";
+// import Footer from "./components/Footer";
 
-function App(){
-  return( 
-    <div className="app">
-      <Navbar/>
-      <Main/>
-      <Sidebar1/>
-      <Sidebar2/>
-      <Footer/>
-    </div> 
-  )
-}
-export default App;
+// function App(){
+//   return( 
+//     <div className="app">
+//       <Navbar/>
+//       <Main/>
+//       <Sidebar1/>
+//       <Sidebar2/>
+//       <Footer/>
+//     </div> 
+//   )
+// }
+// export default App;
 
 
 
+// ! props
+//  import React, { Component } from 'react'
+//  import CBCPropEx1 from './propexample/CBCPropEx1';
+ 
+//  export default class App extends Component {
+//    render() {
+//      return (
+//        <div>
+//         App
+//         <CBCPropEx1
+//         username="Nithin"
+//         age={20}
+//         hobbies={["studying , sleeping, reeding"]}
+//         address={{city:"Hyd" , area:"maisammaguda"}}
+//         isMarried={false}
+//         sendFun={function(){alert("hi nithin")}}
+//         />
+//        </div>
+//      )
+//    }
+//  }
+ 
+
+
+// import React from 'react'
+// import FBCPropex1 from './propexample/FBCPropex1';
+
+// const App = () => {
+
+//   return (
+//     <div>
+//         <FBCPropex1
+//         username = "nithin"
+//         IsLoggedIn={true}
+//         Profile={["instagram","snapchat","facebook","x"]}
+//         />
+//       </div>
+//   )
+// }
+
+// export default App;
+
+
+
+///! props.children
+
+
+  import React from 'react'
+  import PropsChildrenex from './propexample/PropsChildrenex'
+  import SubChildProps from './propexample/SubChildProps'
+  
+  const App = () =>{
+      return (
+        <div>
+          <PropsChildrenex username="nithin" company="Google">
+          <h1>This data is passing as a props to child components</h1>
+          <SubChildProps/>
+          </PropsChildrenex>
+          
+        </div>
+      )
+    }
+   export default App;
---
+++ src/propexample/CBCPropEx1.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class CBCPropEx1 extends Component {
+    render() {
+        console.log(this);
+        return (
+          <div>
+        <h1>{this.props.username}</h1>
+        <h2>{this.props.age}</h2>
+        {
+            this.props.hobbies.map(hobby=>{
+                return <li>{hobby}</li>
+            })
+        }
+        { this.props.isMarried ? <h1>Married</h1> : <h1>Not Marreied</h1>}
+        <button onClick={this.props.sendFun}>Click</button>
+        </div>
+        )
+        
+    }
+}
+export default CBCPropEx1;
